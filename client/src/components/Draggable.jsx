cdc34d94cfc10baf7d8884ce0c26a5503a7870b2
Gavin Matthews <bladedancer.gavin@gmail.com>
2022-05-29 19:57:49 +0100

    Drag and drop.

--- client/src/components/Draggable.jsx
+++ client/src/components/Draggable.jsx
@@ -1,73 +1,90 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { useSigma } from "@react-sigma/core";
-import { Children } from 'react/cjs/react.production.min';
 
-const Draggable = ({children}) => {
-    const sigma = useSigma();
-    let graph = sigma.getGraph();
-    const [dragState, setDragState] = useState({
+const Draggable = ({ children }) => {
+  const sigma = useSigma();
+  let graph = sigma.getGraph();
+  const [dragState, setDragState] = useState({
+    dragging: false,
+    node: null,
+    x: null,
+    y: null,
+  });
+
+  // TODO Use Event
+  const updateableState = useRef(dragState);
+  useEffect(() => {
+    updateableState.current = dragState;
+  });
+
+  // Register the event handlers
+  useEffect(() => {
+    console.log("ADDING LISTENERS");
+    const downNode = (e) => {
+      const dragState = updateableState.current;
+      let ds = {
+        ...dragState,
+        dragging: true,
+        node: e.node,
+      };
+      setDragState(ds);
+
+      // Prevent sigma to move camera:
+      e.preventSigmaDefault();
+      e.event.original.preventDefault();
+      e.event.original.stopPropagation();
+    };
+
+    const mouseMoveBody = (e) => {
+      const dragState = updateableState.current;
+      if (!dragState.dragging || !dragState.node) {
+        return;
+      }
+      const pos = sigma.viewportToGraph(e);
+      setDragState({
+        ...dragState,
+        x: pos.x,
+        y: pos.y,
+      });
+      // Prevent sigma to move camera:
+      e.preventSigmaDefault();
+      e.original.preventDefault();
+      e.original.stopPropagation();
+    };
+
+    const mouseUp = () => {
+      const dragState = updateableState.current;
+      setDragState({
+        ...dragState,
         dragging: false,
-        node: null,
-        x: null,
-        y: null
-    });
+      });
+    };
 
-    useEffect(() => {
-        if (dragState.dragging) {
-            graph.setNodeAttribute(dragState.node, "highlighted", true);
-            if (dragState.x != null) {
-                graph.setNodeAttribute(dragState.node, "x", dragState.x);
-                graph.setNodeAttribute(dragState.node, "y", dragState.y);
-            }
-        } else if (dragState.node) {
-            graph.removeNodeAttribute(dragState.node, "highlighted");
-        }
-    }, [dragState]);
+    const mouseDown = () => {
+      if (!sigma.getCustomBBox()) sigma.setCustomBBox(sigma.getBBox());
+    };
 
-    const downNode = useCallback((e) => {
-        console.log(sigma.viewportToGraph(e))
-        setDragState({
-            ...dragState,
-            dragging: true,
-            node: e.node
-        })
-      }, [setDragState])
+    sigma.on("downNode", downNode);
+    sigma.getMouseCaptor().on("mousemovebody", mouseMoveBody);
+    sigma.getMouseCaptor().on("mouseup", mouseUp);
+    sigma.getMouseCaptor().on("mousedown", mouseDown);
+  }, []);
 
-    const mouseMoveBody = useCallback((e) => {
-        if (!dragState.dragging || !dragState.node) {
-            return;
-        }
-        const pos = sigma.viewportToGraph(e);
-        setDragState({
-            ...dragState,
-            x: pos.x,
-            y: pos.y
-        });
-        // Prevent sigma to move camera:
-        e.preventSigmaDefault();
-        e.original.preventDefault();
-        e.original.stopPropagation();
-    }, [setDragState])
+  // Move the node
+  useEffect(() => {
+    const dragState = updateableState.current;
+    if (dragState.dragging) {
+      graph.setNodeAttribute(dragState.node, "highlighted", true);
+      if (dragState.x != null) {
+        graph.setNodeAttribute(dragState.node, "x", dragState.x);
+        graph.setNodeAttribute(dragState.node, "y", dragState.y);
+      }
+    } else if (dragState.node) {
+      graph.removeNodeAttribute(dragState.node, "highlighted");
+    }
+  }, [dragState, graph]);
 
-    const mouseUp = useCallback(() => {
-        setDragState({
-            ...dragState,
-            dragging: false
-        });
-      }, [setDragState])
-
-    const mouseDown = useCallback(() => {
-        if (!sigma.getCustomBBox()) sigma.setCustomBBox(sigma.getBBox());
-    }, [sigma]);
-
-    useEffect(() => {
-        console.log("ADDING LISTENERS")
-        sigma.on("downNode", downNode);
-        sigma.getMouseCaptor().on("mousemovebody", mouseMoveBody);
-        sigma.getMouseCaptor().on("mouseup", mouseUp);
-        sigma.getMouseCaptor().on("mousedown", mouseDown);
-    }, [sigma, downNode, mouseMoveBody, mouseUp, mouseDown]);
-    return <>{children}</>;
-}
+  return <>{children}</>;
+};
 
 export default Draggable;
