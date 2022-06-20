846569da532167d4835627c98a7aaad556776fba
Gavin Matthews <gmatthews@axway.com>
2022-06-20 18:40:46 +0100

    Scope filter on instances.

--- client/src/components/controls/SummaryControl.jsx
+++ client/src/components/controls/SummaryControl.jsx
@@ -50,10 +50,45 @@ const DefinitionsSummaryControl = ({ nodeData }) => {
 };
 
 const InstancesSummaryControl = ({ resources, nodeData }) => {
+    let [selectedValues, setSelectedValues] = useState([]);
+    const { nodeFilter, setNodeFilter } = useSetNodeFilter();
+
+    console.log(resources, nodeData);
+
+    let scopesByKind = nodeData.scopes.reduce((acc, current) => {
+        acc[current.kind] = acc[current.kind] || [];
+        acc[current.kind].push(current);
+        return acc;
+    }, {});
+
+    Object.keys(scopesByKind)
+        .forEach(s => {
+            scopesByKind[s].sort(
+                (a, b) => a.kind.localeCompare(b.name)
+            );
+        });
+
+    let scopes = Object.values(scopesByKind).flatMap(s => s);
+
+    const onSelect = useCallback((e) => {
+        setNodeFilter({
+            ...nodeFilter,
+            ids: e.map(filterNode => nodeData.nodes.filter(anode => (filterNode.id === anode.id) || (!anode.isScope &&  anode.scope.id === filterNode.id))).flatMap(s=>s).map(n => n.id)
+        })
+    }, [nodeFilter, setNodeFilter]);
+
     return (
-        <>
-            <div>Instances</div>
-        </>
+        <Multiselect
+            options={scopes} // Options to display in the dropdown
+            placeholder="Select Scopes"
+            selectedValues={selectedValues} // Preselected value to persist in dropdown
+            onSelect={onSelect} // Function will trigger on select event
+            onRemove={onSelect} // Function will trigger on remove event
+            displayValue="name" // Property name to display in the dropdown options
+            groupBy="kind"
+            avoidHighlightFirstOption={true}
+            emptyRecordMsg="No scopes found"
+        />
     );
 };
 
