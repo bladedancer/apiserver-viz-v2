103658886486160682a7665ec306edb38fb94dd6
Gavin Matthews <gmatthews@axway.com>
2022-06-23 17:47:14 +0100

    Apply overrides.

--- client/src/components/controls/LayoutControl.jsx
+++ client/src/components/controls/LayoutControl.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState, useMemo } from "react";
+import React, { useCallback, useEffect, useState, useMemo, useRef } from "react";
 import { FiGrid, FiPlay, FiPause, FiSettings } from "react-icons/fi";
 import Form from "@rjsf/core";
 import { useCy } from "../../hooks/useCy";
@@ -12,6 +12,7 @@ import cise from "cytoscape-cise";
 import klay from "cytoscape-klay";
 import euler from "cytoscape-euler";
 import Button from "../utils/Button.jsx"
+import { async } from "regenerator-runtime";
 
 Cytoscape.use(euler);
 Cytoscape.use(klay);
@@ -21,6 +22,169 @@ Cytoscape.use(fcose);
 Cytoscape.use(avsdf);
 Cytoscape.use(COSEBilkent);
 
+const supportedLayouts = {
+  avsdf: {
+    name: "avsdf",
+
+    //https://github.com/iVis-at-Bilkent/cytoscape.js-avsdf
+  },
+  breadthfirst: {
+    name: "breadthfirst",
+    circle: true,
+    avoidOverlap: true,
+    nodeDimensionsIncludeLabels: true,
+    //https://js.cytoscape.org/#layouts/breadthfirst
+
+    schema: {
+      title: "breadthfirst",
+      type: "object",
+      properties: {
+        avoidOverlap: {type: "boolean", title: "Avoid Overlap", default: true},
+        circle: {type: "boolean", title: "Circle", default: true},
+      }
+    }
+  },
+  circle: {
+    name: "circle",
+    nodeDimensionsIncludeLabels: true,
+    //https://js.cytoscape.org/#layouts/circle
+  },
+  cola: {
+    name: "cola",
+    nodeDimensionsIncludeLabels: true,
+    infinite: true,
+    edgeLength: (e) => {
+      let length =
+        e.source().data("groupId") === e.target().data("groupId")
+          ? 100
+          : 300;
+      return length;
+    },
+    convergenceThreshold: 0.1,
+    avoidOverlap: true,
+
+    //randomize: true,
+    fit: false,
+    //https://github.com/cytoscape/cytoscape.js-cola
+    schema: {
+      title: "cola",
+      type: "object",
+      properties: {
+        avoidOverlap: {type: "boolean", title: "Avoid Overlap", default: true},
+        convergenceThreshold: {type: "number", title: "Convergence Threshold", default: 0.1, minimum: 0, maximum: 100,},
+        infinite: {type: "boolean", title: "Infinite", default: true},
+      }
+    }
+  },
+  concentric: {
+    name: "concentric",
+    nodeDimensionsIncludeLabels: true,
+    concentric: (n) => {
+      return n.data("isScope") ? 20 : n.indegree() + 1;
+    },
+    levelWidth: (n) => 1,
+    //equidistant: true
+    // todo: https://js.cytoscape.org/#layouts/concentric
+  },
+  cise: {
+    name: "cise",
+    clusters: (n) => n.data("groupIndex"),
+    allowNodesInsideCircle: true,
+    idealInterClusterEdgeLengthCoefficient: 10,
+    animate: true,
+    fit: false,
+    // https://github.com/iVis-at-Bilkent/cytoscape.js-cise
+  },
+  cose: {
+    name: "cose",
+    nodeDimensionsIncludeLabels: true,
+    // https://js.cytoscape.org/#layouts/cose
+  },
+  "cose-bilkent": {
+    name: "cose-bilkent",
+    // other options
+    padding: 50,
+    nodeDimensionsIncludeLabels: true,
+    idealEdgeLength: 400,
+    edgeElasticity: 0.1,
+    nodeRepulsion: 8500,
+    quality: 'proof',
+    animate: 'during',
+    //https://github.com/cytoscape/cytoscape.js-cose-bilkent
+  },
+  // "d3-force": {
+  //   name: "d3-force",
+  //   link: (d) => d.data("id")
+
+  //   // https://github.com/shichuanpo/cytoscape.js-d3-force
+  // },
+  euler: {
+    name: "euler",
+    springLength: (e) => {
+      let type = e.data("type");
+      if (e.type === "scope") {
+        return 200;
+      } else {
+        return 300;
+      }
+    },
+    mass: (n) => (n.data("isScope") ? 20 : 1),
+    movementThreshold: 0.1,
+    maxIterations: 50000,
+    fit: false,
+    maxSimulationTime: 30000,
+    gravity: -10,
+    //pull: 0.0005,
+
+    // https://github.com/cytoscape/cytoscape.js-euler
+  },
+  fcose: {
+    name: "fcose",
+    nodeDimensionsIncludeLabels: true,
+    packComponents: false,
+    nodeRepulsion: (n) => n.data('isScope') ? 8000 : 4500,
+    idealEdgeLength: (e) => {
+      let type = e.data("type");
+      if (e.type === "scope") {
+        return 50;
+      } else if (e.type === "soft") {
+        return 100;
+      } else {
+        return 300;
+      }
+    },
+    edgeElasticity: (e) => {
+      let type = e.data("type");
+      if (e.type === "scope") {
+        return 0.1;
+      } else if (e.type === "soft") {
+        return 0.9;
+      } else {
+        return 0.45;
+      }
+    },
+    //https://github.com/iVis-at-Bilkent/cytoscape.js-fcose
+  },
+  grid: {
+    name: "grid",
+  },
+  klay: {
+    name: "klay",
+    nodeDimensionsIncludeLabels: true,
+    fit: false,
+    klay: {
+      compactComponents: true,
+      nodeLayering: "NETWORK_SIMPLEX",
+      thoroughness: 20,
+      fixedAlignment: "BALANCED",
+    },
+    //https://github.com/cytoscape/cytoscape.js-klay/blob/master/README.md
+  },
+  random: {
+    name: "random",
+  }
+};
+
 const LayoutControl = ({
   className,
   style,
@@ -29,9 +193,11 @@ const LayoutControl = ({
   const cy = useCy();
   const [opened, setOpened] = useState(false);
   const [layoutConfigOpened, setLayoutConfigOpened] = useState(false);
+  const [layoutOverrides, setLayoutOverrides] = useState({});
   const [layout, setLayout] = useState("breadthfirst");
   const [activeLayout, setActiveLayout] = useState();
   const {contentModifiedTS} = useSetContentModifiedTS();
+  const isMounted = useRef(false);
 
   // Common html props for the div wrapper
   const htmlProps = {
@@ -40,169 +206,13 @@ const LayoutControl = ({
   };
 
   const layouts = useMemo(() => {
-    return {
-      avsdf: {
-        name: "avsdf",
-
-        //https://github.com/iVis-at-Bilkent/cytoscape.js-avsdf
-      },
-      breadthfirst: {
-        name: "breadthfirst",
-        circle: true,
-        avoidOverlap: true,
-        nodeDimensionsIncludeLabels: true,
-        //https://js.cytoscape.org/#layouts/breadthfirst
-
-        schema: {
-          title: "breadthfirst",
-          type: "object",
-          properties: {
-            avoidOverlap: {type: "boolean", title: "Avoid Overlap", default: true},
-            circle: {type: "boolean", title: "Circle", default: true},
-          }
-        }
-      },
-      circle: {
-        name: "circle",
-        nodeDimensionsIncludeLabels: true,
-        //https://js.cytoscape.org/#layouts/circle
-      },
-      cola: {
-        name: "cola",
-        nodeDimensionsIncludeLabels: true,
-        infinite: true,
-        edgeLength: (e) => {
-          let length =
-            e.source().data("groupId") === e.target().data("groupId")
-              ? 100
-              : 300;
-          return length;
-        },
-        convergenceThreshold: 0.1,
-        avoidOverlap: true,
-
-        //randomize: true,
-        fit: false,
-        //https://github.com/cytoscape/cytoscape.js-cola
-        schema: {
-          title: "cola",
-          type: "object",
-          properties: {
-            avoidOverlap: {type: "boolean", title: "Avoid Overlap", default: true},
-            convergenceThreshold: {type: "number", title: "Convergence Threshold", default: 0.1, minimum: 0, maximum: 100,},
-            infinite: {type: "boolean", title: "Infinite", default: true},
-          }
-        }
-      },
-      concentric: {
-        name: "concentric",
-        nodeDimensionsIncludeLabels: true,
-        concentric: (n) => {
-          return n.data("isScope") ? 20 : n.indegree() + 1;
-        },
-        levelWidth: (n) => 1,
-        //equidistant: true
-        // todo: https://js.cytoscape.org/#layouts/concentric
-      },
-      cise: {
-        name: "cise",
-        clusters: (n) => n.data("groupIndex"),
-        allowNodesInsideCircle: true,
-        idealInterClusterEdgeLengthCoefficient: 10,
-        animate: true,
-        fit: false,
-        // https://github.com/iVis-at-Bilkent/cytoscape.js-cise
-      },
-      cose: {
-        name: "cose",
-        nodeDimensionsIncludeLabels: true,
-        // https://js.cytoscape.org/#layouts/cose
-      },
-      "cose-bilkent": {
-        name: "cose-bilkent",
-        // other options
-        padding: 50,
-        nodeDimensionsIncludeLabels: true,
-        idealEdgeLength: 400,
-        edgeElasticity: 0.1,
-        nodeRepulsion: 8500,
-        quality: 'proof',
-        animate: 'during',
-        //https://github.com/cytoscape/cytoscape.js-cose-bilkent
-      },
-      // "d3-force": {
-      //   name: "d3-force",
-      //   link: (d) => d.data("id")
-
-      //   // https://github.com/shichuanpo/cytoscape.js-d3-force
-      // },
-      euler: {
-        name: "euler",
-        springLength: (e) => {
-          let type = e.data("type");
-          if (e.type === "scope") {
-            return 200;
-          } else {
-            return 300;
-          }
-        },
-        mass: (n) => (n.data("isScope") ? 20 : 1),
-        movementThreshold: 0.1,
-        maxIterations: 50000,
-        fit: false,
-        maxSimulationTime: 30000,
-        gravity: -10,
-        //pull: 0.0005,
-
-        // https://github.com/cytoscape/cytoscape.js-euler
-      },
-      fcose: {
-        name: "fcose",
-        nodeDimensionsIncludeLabels: true,
-        packComponents: false,
-        nodeRepulsion: (n) => n.data('isScope') ? 8000 : 4500,
-        idealEdgeLength: (e) => {
-          let type = e.data("type");
-          if (e.type === "scope") {
-            return 50;
-          } else if (e.type === "soft") {
-            return 100;
-          } else {
-            return 300;
-          }
-        },
-        edgeElasticity: (e) => {
-          let type = e.data("type");
-          if (e.type === "scope") {
-            return 0.1;
-          } else if (e.type === "soft") {
-            return 0.9;
-          } else {
-            return 0.45;
-          }
-        },
-        //https://github.com/iVis-at-Bilkent/cytoscape.js-fcose
-      },
-      grid: {
-        name: "grid",
-      },
-      klay: {
-        name: "klay",
-        nodeDimensionsIncludeLabels: true,
-        fit: false,
-        klay: {
-          compactComponents: true,
-          nodeLayering: "NETWORK_SIMPLEX",
-          thoroughness: 20,
-          fixedAlignment: "BALANCED",
-        },
-        //https://github.com/cytoscape/cytoscape.js-klay/blob/master/README.md
-      },
-      random: {
-        name: "random",
-      },
-    };
-  });
+    // Merge the overrides with the defaults
+    Object.keys(layoutOverrides).forEach(k => {
+      supportedLayouts[k] = {...supportedLayouts[k], ...layoutOverrides[k]}
+    });
+    console.log(layoutOverrides, supportedLayouts);
+    return supportedLayouts;
+  }, [layoutOverrides]);
 
   const runLayout = useCallback(async () => {
     if (activeLayout) {
@@ -244,6 +254,14 @@ const LayoutControl = ({
     setActiveLayout(null);
   }, [activeLayout, setActiveLayout]);
 
+  // When the overrides whindow closes rerun layout
+  useEffect(async () => {
+    if (isMounted.current) {
+      !layoutConfigOpened && runLayout();
+    } else {
+      isMounted.current = true;
+    }
+  }, [layoutConfigOpened, setLayoutConfigOpened])
 
   return (
     <>
@@ -284,7 +302,16 @@ const LayoutControl = ({
         )}
         {layoutConfigOpened === true && (
           <div className="layout-config">
-            <Form schema={layouts[layout].schema} />
+            <Form
+              schema={layouts[layout].schema}
+              formData={layouts[layout]}
+              onSubmit={({formData}, e) => {
+                setLayoutOverrides({
+                  [layout]: formData
+                });
+                setLayoutConfigOpened(false);
+              }}
+            />
           </div>
         )}
       </div>
